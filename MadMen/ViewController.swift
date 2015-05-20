1250b7f62631073f8bf938e273042fbe9e103ec5
ritmatter <mritter123@gmail.com>
2015-05-19 20:24:27 -0400

    Initial Commit

---
+++ MadMen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MadMen
+//
+//  Created by ritmatter on 5/19/15.
+//  Copyright (c) 2015 ritmatter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
